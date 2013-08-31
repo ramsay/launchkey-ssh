68a3ba9ed4438c4d4b4e1213a8149a05d3215a1c
Robert Ramsay <robert.alan.ramsay@gmail.com>
2013-08-31 13:13:19 -0700

    Working python prototype, only works for one launchkey user.

--- pam_launchkey.py
+++ pam_launchkey.py
@@ -10,31 +10,36 @@ add the following line to /etc/pam.d/sshd
 account required pam_python.so /path/to/pam_launchkey.pam_python
 
 """
+import sys, os
+root_dir = os.sep.join(__file__.split(os.sep)[:-1])
+sys.path.append(root_dir + '/.env/lib/python2.7/site-packages/')
 import launchkey
 from time import sleep 
 DEFAULT_USER    = "nobody"
 
 
 def pam_sm_acct_mgmt(pamh, flags, argv):
-    response = pamh.conversation(pamh.Message(0, "LaunchKey Username:"))
-    username = response.resp
+    username = argv[1]
     if login(username):
         return pamh.PAM_SUCCESS
-    return pamh.PAM_AUTH_ERROR
+    return pamh.PAM_AUTH_ERR
 
 
 def login(username):
     app_key = 1301024551
-    secret_key = open("secret_key", "r").read()
-    private_key = open("private.key", "r").read() 
+    secret_key = open(root_dir + "/secret.key", "r").read().strip()
+    private_key = open(root_dir + "/private.key", "r").read()
+    print >> sys.stderr, (app_key, secret_key, private_key) 
     api = launchkey.API(app_key, secret_key, private_key)
 
     auth_request = api.authorize(username)
-    
+    print >> sys.stderr, "auth_request: ", auth_request
     auth_response = {}
-
+    retries = 0
     while auth_response.get('auth') is None:
         sleep(5)
         auth_response = api.poll_request(auth_request)
+        print >> sys.stderr, (retries, auth_response)
 
-    return api.is_authorized(auth_response['auth'])
+    return api.is_authorized(auth_request, auth_response['auth'])
+
