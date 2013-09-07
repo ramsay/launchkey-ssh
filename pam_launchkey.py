7dcaa27e208dda3632cbe132cee654fea5db47fb
Robert Ramsay <robert.alan.ramsay@gmail.com>
2013-09-07 09:48:24 -0700

    Change from account to authenticate

--- pam_launchkey.py
+++ pam_launchkey.py
@@ -7,34 +7,50 @@ pam_python http://ace-host.stuart.id.au/russell/files/pam_python/
 
 Install pam_python
 add the following line to /etc/pam.d/sshd
-account required pam_python.so /path/to/pam_launchkey.pam_python
+auth required pam_python.so /path/to/pam_launchkey.py
+
+sshd_config:
+ChallengeResponseAuthentication yes
 
 """
+import sys
+sys.path.append('/home/robert/workspace/ramsay/launchkey-ssh/.env/lib/python2.7/site-packages/')
 import launchkey
 from time import sleep 
-DEFAULT_USER    = "nobody"
 
 
-def pam_sm_acct_mgmt(pamh, flags, argv):
-    response = pamh.conversation(pamh.Message(0, "LaunchKey Username:"))
-    username = response.resp
-    if login(username):
-        return pamh.PAM_SUCCESS
-    return pamh.PAM_AUTH_ERROR
+def pam_sm_authenticate(pamh, flags, argv):
+    response = pamh.conversation(message)
+    message = pamh.Message(pamh.PAM_PROMPT_ECHO_ON, "LaunchKey username: ")
+    try:
+        response = pamh.conversation(message)
+    except pamh.exception, exc:
+        print exc
+    else:
+        username = response.resp
+        pamh.Message(pamh.PAM_TEXT_INFO, "")
+        if login(username):
+            return pamh.PAM_SUCCESS
+    return pamh.PAM_AUTH_ERR
 
 
 def login(username):
     app_key = 1301024551
-    secret_key = open("secret_key", "r").read()
-    private_key = open("private.key", "r").read() 
+    secret_key = open("secret.key", "r").read().strip()
+    private_key = open("private.key", "r").read()
     api = launchkey.API(app_key, secret_key, private_key)
 
     auth_request = api.authorize(username)
-    
     auth_response = {}
-
+    retries = 0
     while auth_response.get('auth') is None:
         sleep(5)
         auth_response = api.poll_request(auth_request)
+        print >> sys.stderr, (retries, auth_response)
 
-    return api.is_authorized(auth_response['auth'])
+    return api.is_authorized(auth_request, auth_response['auth'])
+
+if __name__ == '__main__':
+    api = None
+    print login('rramsay')
+
